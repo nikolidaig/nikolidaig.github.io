062f92be9cd24b8d39c5b11e72081da1743187cd
Nikoli Daigneault <nikolidaig@proton.me>
2023-12-02 18:51:59 -0500

    Remove all character

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nikolidaig.github.io',
   prefetch: true,
-  integrations: [mdx()]
+  integrations: [mdx(), tailwind()]
 });
