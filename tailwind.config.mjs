8511d51b1ddc2c1996d783ff7e12c87fa0a795e9
Nikoli Daigneault <nikolidaig@proton.me>
2024-05-07 20:30:53 -0400

    Remove condensed font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			serif: ['"IBM Plex Serif"', 'sans-serif'],
 			sans: ['"IBM Plex Sans"', 'sans-serif'],
-			condensed: ['"IBM Plex Sans Condensed"', 'sans-serif'],
 		},
 	},
 	plugins: [
